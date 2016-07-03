f7a552c32ff3e7ed34d59e62606887fa70ea9069
italopvm <italopvm@gmail.com>
2016-07-03 13:01:40 -0600

    second commit

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ function anyWord(){
 	var word = prompt("enter any word");
 	var length = word.length;
 	var third = word.charAt(2);
+	var lowercase = word.toLowerCase();
+	var uppercase = word.toUpperCase();
+	var sub = word.substring(1,5);
+
+
 
 	alert("This is your word: " + word + " " +
 		  "This is the length of your word: " + length + " " +
-		  "This is the third character of your word: " + third );
+		  "This is the third character of your word: " + third + " " +
+		  "lowercase: " + lowercase + " " + "uppercase: " + uppercase +
+		  " " + "I like to practice with your " + word + 
+		  "This is the substring of the word " + sub);
 
 
 }
