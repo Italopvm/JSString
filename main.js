2ce82a55a362240ab3db838926dbc79305c53e31
italopvm <italopvm@gmail.com>
2016-07-03 12:19:09 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function anyWord(){
+
+	var word = prompt("enter any word");
+	var length = word.length;
+	var third = word.charAt(2);
+
+	alert("This is your word: " + word + " " +
+		  "This is the length of your word: " + length + " " +
+		  "This is the third character of your word: " + third );
+
+
+}
